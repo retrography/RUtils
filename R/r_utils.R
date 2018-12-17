b3aa5f009d5873150c13fa84e5d5425649c27b2e
Mahmood S. Zargar <mahmood@gmail.com>
2018-12-17 14:26:01 +0100

    clip.output

--- R/r_utils.R
+++ R/r_utils.R
@@ -1,3 +1,52 @@
+#TODO: Mostly copied from R code
+#' @export
+clip.output <- function (..., type = c("output", "message")) 
+{
+ 	append <- FALSE
+	split <- FALSE
+	file = pipe("pbcopy")
+ 	args <- substitute(list(...))[-1L]
+    type <- match.arg(type)
+    rval <- NULL
+    closeit <- TRUE
+    if (is.null(file)) 
+        file <- textConnection("rval", "w", local = TRUE)
+    else if (is.character(file)) 
+        file <- file(file, if (append) 
+            "a"
+        else "w")
+    else if (inherits(file, "connection")) {
+        if (!isOpen(file)) 
+            open(file, if (append) 
+                "a"
+            else "w")
+        else closeit <- FALSE
+    }
+    else stop("'file' must be NULL, a character string or a connection")
+    sink(file, type = type, split = split)
+    on.exit({
+        sink(type = type, split = split)
+        if (closeit) close(file)
+    })
+    pf <- parent.frame()
+    evalVis <- function(expr) withVisible(eval(expr, pf))
+    for (i in seq_along(args)) {
+        expr <- args[[i]]
+        tmp <- switch(mode(expr), expression = lapply(expr, evalVis), 
+            call = , name = list(evalVis(expr)), stop("bad argument"))
+        for (item in tmp) if (item$visible) 
+            print(item$value)
+    }
+    on.exit()
+    sink(type = type, split = split)
+    if (closeit) 
+        close(file)
+    if (is.null(rval)) 
+        invisible(NULL)
+    else rval
+}
+# clip.output <- function(...) {capture.output(..., file = pipe("pbcopy"))}
+
 #' @export
 rm.all <-
   function() {
